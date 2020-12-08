ae4d8b6733ac26e151fdcf7c2f362bfba5e0fd04
Md. Babul Hasan (Noyen) <bhyean16@cse.pstu.ac.bd>
2020-12-08 12:45:54 +0600

    fibo update

--- Searching/8 Fibonacci Search.cpp
+++ Searching/8 Fibonacci Search.cpp
@@ -4,42 +4,76 @@
                      Faculty of Computer Science and Engineering
 /*===========================================================================================**/
 #include <iostream>
-#include <cstdlib>
-#include <stdio.h>
-#include <assert.h>
-#include <math.h>
-using namespace std;
 
-int FibonacciSearch(int arr[],int Size,int key)
+
+int min(int x, int y) { return (x<=y)? x : y; }
+
+int fibMonaccianSearch(int arr[], int x, int n)
 {
-    int Start,End;
-    Start = 0;
-    End = sqrt(Size);
+    int fibMMm2 = 0;   // (m-2)'th Fibonacci No.
+    int fibMMm1 = 1;   // (m-1)'th Fibonacci No.
+    int fibM = fibMMm2 + fibMMm1;  // m'th Fibonacci
 
-    while(arr[End]< key && End<Size)
+    while (fibM < n)
     {
-        Start = End;
-        End += sqrt(Size);
-        if(End > Size-1)
-        {
-            End = Size;
-        }
+        fibMMm2 = fibMMm1;
+        fibMMm1 = fibM;
+        fibM  = fibMMm2 + fibMMm1;
     }
 
-    for(int i=Start; i<End ;i++)
+    printf("Before loop\n");
+    printf("fibMMm2 = %d\nfibMMm1 = %d\nfibM = %d\n\n",fibMMm2,fibMMm1,fibM);
+
+
+    int offset = -1;
+
+    printf("Entering the loop:\n");
+
+    while (fibM > 1)
     {
-        if(arr[i] == key)
+        int i = min(offset+fibMMm2, n-1);
+
+        printf("i %d\n",i);
+
+        if (arr[i] < x)
         {
-            return i;
+            printf("1st condition:\n");
+            fibM  = fibMMm1;
+            fibMMm1 = fibMMm2;
+            fibMMm2 = fibM - fibMMm1;
+            offset = i;
+            printf("Offset %d\n",offset);
+
+            printf("fibMMm2 = %d\nfibMMm1 = %d\nfibM = %d\n\n",fibMMm2,fibMMm1,fibM);
+
         }
+
+        else if (arr[i] > x)
+        {
+            printf("2nd condition:\n");
+
+            fibM  = fibMMm2;
+            fibMMm1 = fibMMm1 - fibMMm2;
+            fibMMm2 = fibM - fibMMm1;
+            printf("fibMMm2 = %d\nfibMMm1 = %d\nfibM = %d\n\n",fibMMm2,fibMMm1,fibM);
+
+        }
+
+        else return i;
     }
+
+    if(fibMMm1 && arr[offset+1]==x)return offset+1;
+
     return -1;
 }
 
-int main()
+int main(void)
 {
-    int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    int found = FibonacciSearch(arr,9,5);
-    cout<<found<<endl;
+    int arr[] = {1, 3, 6, 9, 12, 16, 20, 25,26,27,28,29,93,100};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x = 93;
+
+    printf("Found at index: %d", fibMonaccianSearch(arr, x, n));
+    system("pause");
     return 0;
 }
